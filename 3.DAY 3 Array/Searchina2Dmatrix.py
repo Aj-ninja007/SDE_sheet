b30fdba5a57536073033926e66dbc3be639eb09f
Prince mishra <113469532+Aj-ninja007@users.noreply.github.com>
2024-08-07 00:03:13 +0530

    Update Searchina2Dmatrix.py

--- 3.DAY 3 Array/Searchina2Dmatrix.py
+++ 3.DAY 3 Array/Searchina2Dmatrix.py
@@ -1,1 +1,10 @@
-
+#https://leetcode.com/problems/search-a-2d-matrix/
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        res=[]
+        for i in matrix:
+            res+=i
+        for i in res:
+            if target in res:
+                return True
+        return False
