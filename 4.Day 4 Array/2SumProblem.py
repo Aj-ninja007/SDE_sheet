6d74c8e774fb2c60c7eada4f52cc3a836b27a29c
Prince mishra <113469532+Aj-ninja007@users.noreply.github.com>
2024-08-09 23:30:45 +0530

    Create 2SumProblem.py

---
+++ 4.Day 4 Array/2SumProblem.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l=[]
+        
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    l.append(i)
+                    l.append(j)
+                    print(l)
+                    return l
+                
+            
+
+        
+     
+
+        
